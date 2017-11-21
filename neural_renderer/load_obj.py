3b6e6c9aea50db55287177b2daa7d9d167dedc85
hkato <hiroharu.kato.1989.10.13@gmail.com>
2017-11-21 12:13:41 +0900

    add load_obj

---
+++ neural_renderer/load_obj.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+
+def load_obj(filename, normalization=True):
+    """
+    Load Wavefront .obj file.
+    This function only supports vertices (v x x x) and faces (f x x x).
+    """
+
+    # load vertices
+    vertices = []
+    for line in open(filename).readlines():
+        if line.split()[0] == 'v':
+            vertices.append([float(v) for v in line.split()[1:4]])
+    vertices = np.vstack(vertices).astype('float32')
+
+    # load faces
+    faces = []
+    for line in open(filename).readlines():
+        if line.split()[0] == 'f':
+            vs = line.split()[1:]
+            nv = len(vs)
+            v0 = int(vs[0].split('/')[0])
+            for i in range(nv - 2):
+                v1 = int(vs[i + 1].split('/')[0])
+                v2 = int(vs[i + 2].split('/')[0])
+                faces.append((v0, v1, v2))
+    faces = np.vstack(faces).astype('int32') - 1
+
+    # normalize into a unit cube centered zero
+    if normalization:
+        vertices -= vertices.min(0)[None, :]
+        vertices /= np.abs(vertices).max()
+        vertices -= 0.5
+
+    return vertices, faces
