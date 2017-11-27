e989efc9e8ec5d0a21b8008005c3f30aac43f013
hkato <hiroharu.kato.1989.10.13@gmail.com>
2017-11-27 22:43:04 +0900

    backward of rasterize_silhouettes and its test

--- neural_renderer/rasterize_silhouettes.py
+++ neural_renderer/rasterize_silhouettes.py
@@ -25,14 +25,13 @@ class RasterizeSilhouette(chainer.Function):
 
     def forward_gpu(self, inputs):
         xp = chainer.cuda.get_array_module(inputs[0])
-        inputs = [xp.ascontiguousarray(i) for i in inputs]
-        faces, = inputs
-        bs, nf = faces.shape[:2]
-        is_ = self.image_size
+        faces = xp.ascontiguousarray(inputs[0])
+        batch_size, num_faces = faces.shape[:2]
+        image_size = self.image_size
 
         # initialize buffers
-        self.face_index_map = xp.ascontiguousarray(-1 * xp.ones((bs, is_, is_), dtype='int32'))
-        self.images = xp.ascontiguousarray(xp.zeros((bs, is_, is_), dtype='float32'))
+        self.face_index_map = xp.ascontiguousarray(-1 * xp.ones((batch_size, image_size, image_size), dtype='int32'))
+        self.images = xp.ascontiguousarray(xp.zeros((batch_size, image_size, image_size), dtype='float32'))
 
         # vertices -> face_index_map, z_map
         # face_index_map = -1 if background
@@ -46,10 +45,10 @@ class RasterizeSilhouette(chainer.Function):
                 const int is2 = is * is;                    // number of pixels
                 const int pi = i;                           // pixel index on all batches
                 const int bn = pi / (is2);                  // batch number
-                const int yi = (pi % (is2)) / is;           // index of current y-position [0, is - 1]
-                const int xi = (pi % (is2)) % is;           // index of current x-position [0, is - 1]
-                const float yp = (1 - 1. / is) * ((2. / (is - 1)) * yi - 1);   // coordinate of y-position [-1, 1]
-                const float xp = (1 - 1. / is) * ((2. / (is - 1)) * xi - 1);   // coordinate of x-position [-1, 1]
+                const int pyi = (pi % (is2)) / is;          // index of current y-position [0, is - 1]
+                const int pxi = (pi % (is2)) % is;          // index of current x-position [0, is - 1]
+                const float py = (1 - 1. / is) * ((2. / (is - 1)) * pyi - 1);   // coordinate of y-position [-1, 1]
+                const float px = (1 - 1. / is) * ((2. / (is - 1)) * pxi - 1);   // coordinate of x-position [-1, 1]
 
                 /* for each face */
                 float* face;            // current face
@@ -69,13 +68,13 @@ class RasterizeSilhouette(chainer.Function):
                     const float z[3] = {face[2], face[5], face[8]};
 
                     /* check too close & too far */
-                    if (z[0] < 0 || z[1] < 0 || z[2] < 0) continue;
+                    if (z[0] <= 0 || z[1] <= 0 || z[2] <= 0) continue;
                     if (z_min < z[0] && z_min < z[1] && z_min < z[2]) continue;
 
-                    /* check [yp, xp] is inside the face */
-                    if (((yp - y[0]) * (x[1] - x[0]) < (xp - x[0]) * (y[1] - y[0])) ||
-                        ((yp - y[1]) * (x[2] - x[1]) < (xp - x[1]) * (y[2] - y[1])) ||
-                        ((yp - y[2]) * (x[0] - x[2]) < (xp - x[2]) * (y[0] - y[2]))) continue;
+                    /* check [py, px] is inside the face */
+                    if (((py - y[0]) * (x[1] - x[0]) < (px - x[0]) * (y[1] - y[0])) ||
+                        ((py - y[1]) * (x[2] - x[1]) < (px - x[1]) * (y[2] - y[1])) ||
+                        ((py - y[2]) * (x[0] - x[2]) < (px - x[2]) * (y[0] - y[2]))) continue;
 
                     /* compute f_inv */
                     float f_inv[9] = {
@@ -87,7 +86,7 @@ class RasterizeSilhouette(chainer.Function):
 
                     /* compute w = f_inv * p */
                     float w[3];
-                    for (int k = 0; k < 3; k++) w[k] = f_inv[3 * k + 0] * xp + f_inv[3 * k + 1] * yp + f_inv[3 * k + 2];
+                    for (int k = 0; k < 3; k++) w[k] = f_inv[3 * k + 0] * px + f_inv[3 * k + 1] * py + f_inv[3 * k + 2];
 
                     /* sum(w) -> 1, 0 < w < 1 */
                     float w_sum = 0;
@@ -96,7 +95,7 @@ class RasterizeSilhouette(chainer.Function):
                         if (1 < w[k]) w[k] = 1;
                         w_sum += w[k];
                     }
-                    if (1 < w_sum) for (int k = 0; k < 3; k++) w[k] /= w_sum;
+                    for (int k = 0; k < 3; k++) w[k] /= w_sum;
 
                     /* compute 1 / zp = sum(w / z) & check z-buffer */
                     const float zp = 1. / (w[0] / z[0] + w[1] / z[1] + w[2] / z[2]);
@@ -115,12 +114,152 @@ class RasterizeSilhouette(chainer.Function):
                 }
             ''',
             'function',
-        )(faces, nf, is_, self.near, self.far, self.face_index_map.ravel(), self.images.ravel())
+        )(faces, num_faces, image_size, self.near, self.far, self.face_index_map.ravel(), self.images.ravel())
 
         return self.images,
 
     def backward_gpu(self, inputs, grad_outputs):
-        raise NotImplementedError
+        xp = chainer.cuda.get_array_module(inputs[0])
+        faces = xp.ascontiguousarray(inputs[0])
+        grad_images = xp.ascontiguousarray(grad_outputs[0])
+        grad_faces = xp.ascontiguousarray(xp.zeros_like(faces, dtype='float32'))
+        num_faces = faces.shape[1]
+        image_size = self.image_size
+
+        # pseudo gradient
+        chainer.cuda.elementwise(
+            'raw float32 faces, raw int32 face_index_map, raw float32 images, ' +
+            'float32 grad_images, int32 image_size, int32 num_faces, float32 eps ',
+            'raw float32 grad_faces',
+            '''
+                /* exit if no gradient from upper layers */
+                if (grad_images == 0) return;
+
+                /* compute current position & index */
+                const int nf = num_faces;
+                const int is = image_size;
+                const int is2 = is * is;                    // number of pixels
+                const int pi = i;                           // pixel index on all batches
+                const int bn = pi / (is2);                  // batch number
+                const int pyi = (pi % (is2)) / is;          // index of current y-position [0, is]
+                const int pxi = (pi % (is2)) % is;          // index of current x-position [0, is]
+                const float py = (1 - 1. / is) * ((2. / (is - 1)) * pyi - 1);   // coordinate of y-position [-1, 1]
+                const float px = (1 - 1. / is) * ((2. / (is - 1)) * pxi - 1);   // coordinate of x-position [-1, 1]
+
+                const int pfn = face_index_map[pi];        // face number of current position
+                const float pp = images[pi];                // pixel intensity of current position
+
+                for (int axis = 0; axis < 2; axis++) {
+                    for (int direction = -1; direction <= 1; direction += 2) {
+                        int qfn_last = pfn;
+                        for (int d_pq = 1; d_pq < is; d_pq++) {
+                            int qxi, qyi;
+                            float qx, qy;
+                            if (axis == 0) {
+                                qxi = pxi + direction * d_pq;
+                                qyi = pyi;
+                                qx = (1 - 1. / is) * ((2. / (is - 1)) * qxi - 1);
+                                qy = py;
+                                if (qxi < 0 || is <= qxi) break;
+                            } else {
+                                qxi = pxi;
+                                qyi = pyi + direction * d_pq;
+                                qx = px;
+                                qy = (1 - 1. / is) * ((2. / (is - 1)) * qyi - 1);
+                                if (qyi < 0 || is <= qyi) break;
+                            }
+
+                            const int qi = bn * is2 + qyi * is + qxi;
+                            const float qp = images[qi];
+                            const float diff = qp - pp;
+                            const int qfn = face_index_map[qi];
+
+                            if (diff == 0) continue;                    // continue if same pixel value
+                            if (0 <= diff * grad_images) continue;      // continue if wrong gradient
+                            if (qfn == qfn_last) continue;              // continue if p & q are on same face
+
+                            /* adjacent point to check edge */
+                            int rxi, ryi;
+                            float rx, ry;
+                            if (axis == 0) {
+                                rxi = qxi - direction;
+                                ryi = pyi;
+                                rx = (1 - 1. / is) * ((2. / (is - 1)) * rxi - 1);
+                                ry = py;
+                            } else {
+                                rxi = pxi;
+                                ryi = qyi - direction;
+                                rx = px;
+                                ry = (1 - 1. / is) * ((2. / (is - 1)) * ryi - 1);
+                            }
+
+                            for (int mode = 0; mode < 2; mode++) {
+                                float* face;
+                                float* grad_face;
+                                if (mode == 0) {
+                                    if (qfn < 0) continue;
+                                    face = &faces[(bn * nf + qfn) * 3 * 3];
+                                    grad_face = &grad_faces[(bn * nf + qfn) * 3 * 3];
+                                } else if (mode == 1) {
+                                    if (qfn_last != pfn) continue;
+                                    face = &faces[(bn * nf + pfn) * 3 * 3];
+                                    grad_face = &grad_faces[(bn * nf + pfn) * 3 * 3];
+                                }
+
+                                /* for each edge */
+                                for (int vi0 = 0; vi0 < 3; vi0++) {
+                                    /* get vertices */
+                                    int vi1 = (vi0 + 1) % 3;
+                                    float* v0 = &face[vi0 * 3];
+                                    float* v1 = &face[vi1 * 3];
+
+                                    /* get cross point */
+                                    float sx, sy;
+                                    if (axis == 0) {
+                                        sx = (py - v0[1]) * (v1[0] - v0[0]) / (v1[1] - v0[1]) + v0[0];
+                                        sy = py;
+                                    } else {
+                                        sx = px;
+                                        sy = (px - v0[0]) * (v1[1] - v0[1]) / (v1[0] - v0[0]) + v0[1];
+                                    }
+
+                                    /* continue if not cross edge */
+                                    if ((rx < sx) != (sx < qx)) continue;
+                                    if ((ry < sy) != (sy < qy)) continue;
+                                    if ((v0[1] < sy) != (sy < v1[1])) continue;
+                                    if ((v0[0] < sx) != (sx < v1[0])) continue;
+
+                                    /* signed distance (positive if pi < qi) */
+                                    float dist_v0, dist_v1;
+                                    if (axis == 0) {
+                                        dist_v0 = (px - sx) * (v1[1] - v0[1]) / (v1[1] - py);
+                                        dist_v1 = (px - sx) * (v0[1] - v1[1]) / (v0[1] - py);
+                                    } else {
+                                        dist_v0 = (py - sy) * (v1[0] - v0[0]) / (v1[0] - px);
+                                        dist_v1 = (py - sy) * (v0[0] - v1[0]) / (v0[0] - px);
+                                    }
+
+                                    /* add small epsilon */
+                                    dist_v0 = (0 < dist_v0) ? dist_v0 + eps : dist_v0 - eps;
+                                    dist_v1 = (0 < dist_v1) ? dist_v1 + eps : dist_v1 - eps;
+
+                                    /* gradient */
+                                    const float g_v0 = grad_images * diff / dist_v0;
+                                    const float g_v1 = grad_images * diff / dist_v1;
+
+                                    atomicAdd(&grad_face[vi0 * 3 + axis], g_v0);
+                                    atomicAdd(&grad_face[vi1 * 3 + axis], g_v1);
+                                }
+                            }
+                            qfn_last = qfn;
+                        }
+                    }
+                }
+            ''',
+            'function',
+        )(faces, self.face_index_map, self.images, grad_images.ravel(), image_size, num_faces, self.eps, grad_faces)
+
+        return grad_faces,
 
     def forward_cpu(self, inputs):
         raise NotImplementedError
--- tests/test_rasterize_silhouettes.py
+++ tests/test_rasterize_silhouettes.py
@@ -1,17 +1,18 @@
+import chainer
+import chainer.functions as cf
+import chainer.gradient_check
+import chainer.testing
+import cupy as cp
 import numpy as np
 import scipy.misc
 import unittest
 
-import chainer
-import chainer.gradient_check
-import chainer.testing
-
 import neural_renderer
 
 
-class TestRasterizeSilhouette(unittest.TestCase):
+class TestRasterizeSilhouettes(unittest.TestCase):
     def test_case1(self):
-        """Comparison with blender."""
+        # load teapot
         vertices, faces = neural_renderer.load_obj('./tests/data/teapot.obj')
         vertices = vertices[None, :, :]
         faces = faces[None, :, :]
@@ -20,18 +21,76 @@ class TestRasterizeSilhouette(unittest.TestCase):
 
         eye = chainer.cuda.to_gpu(np.array([0, 0, -2.732], 'float32'))
 
+        # transform
         vertices = neural_renderer.look_at(vertices, eye)
         vertices = neural_renderer.perspective(vertices)
         faces = neural_renderer.vertices_to_faces(vertices, faces)
+
+        # rasterize
         images = neural_renderer.rasterize_silhouettes(faces, anti_aliasing=False)
         images = images.data.get()
         image = images[0]
 
+        # load reference image by blender
         ref = scipy.misc.imread('./tests/data/teapot_blender.png')
         ref = ref.astype('float32')
         ref = (ref.min(-1) != 255).astype('float32')
 
         chainer.testing.assert_allclose(ref, image)
+
+    def test_backward_case1(self):
+        faces = [
+            [0.8, 0.8, 1.],
+            [0.0, -0.5, 1.],
+            [0.2, -0.4, 1.]]
+        pxi = 35
+        pyi = 25
+        options = {'image_size': 64, 'anti_aliasing': False}
+
+        faces = cp.array(faces, 'float32')
+        faces = chainer.Variable(faces)
+        images = neural_renderer.rasterize_silhouettes(faces[None, None, :, :], **options)
+        loss = cf.sum(cf.absolute(images[:, pyi, pxi] - 1))
+        loss.backward()
+
+        for i in range(3):
+            for j in range(2):
+                axis = 'x' if j == 0 else 'y'
+                faces2 = cp.copy(faces.data)
+                faces2[i, j] -= 1. / faces.grad[i, j]
+                images = neural_renderer.rasterize_silhouettes(faces2[None, None, :, :], **options)
+                image = np.tile(images[0].data.get()[:, :, None], (1, 1, 3))
+                image[pyi, pxi] = [1, 0, 0]
+                ref = scipy.misc.imread('./tests/data/rasterize_silhouettes_case1_v%d_%s.png' % (i, axis))
+                ref = ref.astype('float32') / 255
+                chainer.testing.assert_allclose(ref, image)
+
+    def test_backward_case2(self):
+        faces = [
+            [0.8, 0.8, 1.],
+            [-0.5, -0.8, 1.],
+            [0.8, -0.8, 1.]]
+        pyi = 40
+        pxi = 50
+        options = {'image_size': 64, 'anti_aliasing': False}
+
+        faces = cp.array(faces, 'float32')
+        faces = chainer.Variable(faces)
+        images = neural_renderer.rasterize_silhouettes(faces[None, None, :, :], **options)
+        loss = cf.sum(cf.absolute(images[:, pyi, pxi]))
+        loss.backward()
+
+        for i in range(3):
+            for j in range(2):
+                axis = 'x' if j == 0 else 'y'
+                faces2 = cp.copy(faces.data)
+                faces2[i, j] -= 1. / faces.grad[i, j]
+                images = neural_renderer.rasterize_silhouettes(faces2[None, None, :, :], **options)
+                image = np.tile(images[0].data.get()[:, :, None], (1, 1, 3))
+                image[pyi, pxi] = [1, 0, 0]
+                ref = scipy.misc.imread('./tests/data/rasterize_silhouettes_case2_v%d_%s.png' % (i, axis))
+                ref = ref.astype('float32') / 255
+                chainer.testing.assert_allclose(ref, image)
 
 
 if __name__ == '__main__':
