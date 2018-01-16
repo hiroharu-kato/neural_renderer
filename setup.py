b021d8d3132a28174cca205128577316abb76156
hkato <kato@mi.t.u-tokyo.ac.jp>
2018-01-16 11:52:58 +0900

    fix setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     name='neural_renderer',
     version='0.0.1',
-    test_suite='tests'
+    test_suite='tests',
     packages=['neural_renderer'],
     install_requires=['numpy', 'chainer', 'cupy'],
 )
