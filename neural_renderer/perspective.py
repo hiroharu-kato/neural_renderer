6b656fafc4632b5678221328ae05431cffae56cf
hkato <hiroharu.kato.1989.10.13@gmail.com>
2018-02-14 17:33:37 +0900

    use chainer.Variable in perspective

--- neural_renderer/perspective.py
+++ neural_renderer/perspective.py
@@ -2,10 +2,13 @@ import chainer
 import chainer.functions as cf
 
 
-def perspective(vertices, angle=30):
+def perspective(vertices, angle=30.):
     assert (vertices.ndim == 3)
     xp = chainer.cuda.get_array_module(vertices)
-    angle = xp.array([xp.radians(angle)] * vertices.shape[0], 'float32')
+    if isinstance(angle, float) or isinstance(angle, int):
+        angle = chainer.Variable(xp.array(angle, 'float32'))
+    angle = angle / 180. * 3.1416
+    angle = cf.broadcast_to(angle[None], (vertices.shape[0], ))
 
     width = cf.tan(angle)
     width = cf.broadcast_to(width[:, None], vertices.shape[:2])
